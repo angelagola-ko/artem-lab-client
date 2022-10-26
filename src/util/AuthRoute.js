ce73871c8ac161aff964dc926df704f3acb51522
Angela Gola <angelagola.ko@gmail.com>
2022-10-26 17:24:19 -0400

    Fixed AuthRoute

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
-import { Route, Redirect } from 'react-router-dom'
-import { AuthContext } from '../context/auth'
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../context/auth';
 
 function AuthRoute({ component: Component, ...rest }){
     const { user } = useContext(AuthContext);
@@ -8,7 +8,7 @@ function AuthRoute({ component: Component, ...rest }){
     return (
         <Route 
             {...rest}
-            render={props =>
+            render={(props) =>
                  user ? <Redirect to="/"/> : <Component {...props}/>
             }
             />
